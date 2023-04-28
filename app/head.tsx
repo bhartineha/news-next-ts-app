a643d9c96027bbcf5f6f3fceef65921f2ba4d9b0
Neha Bharti <neharoy1989@gmail.com>
2023-04-28 18:58:41 +1000

    added app folder with layout

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Head() {
+  return (
+    <>
+      <title>Live News</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      <link></link>
+    </>
+  )
+}
---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../styles/globals.css'
+import Header from './Header';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <head />
+      <body>
+        <Header />
+        <div>{children}</div></body>
+    </html>
+  )
+}
